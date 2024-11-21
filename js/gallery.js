256068d2d6498fe43afa47a3d8936a234cfbea8a
Tyler Wang <wangtyler123@gmail.com>
2024-11-20 18:28:31 -0600

    Updated image browser.

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,11 @@
 
-function generateGallery(album, production, id, photopath, url) {
+function generateGallery(album, production, id, photopath, url, imagePath, albumlink = true) {
 	$.getJSON(album, (file) => {
 		let albumOuter = document.getElementById(id);
 
-		albumOuter.innerHTML = `<h2>${file.album_title}</h2>`;
+		albumOuter.innerHTML = albumlink ? `<h2>
+			<a href="${url}/album?album=${imagePath}" class="gallery-album-link">
+			${file.album_title}<a></h2>`: ``;
 		albumOuter.innerHTML += `<div class="photo-gallery-album-inner" id="inner-${id}"></div>`;
 
 		let albumDiv = document.getElementById(`inner-${id}`)
@@ -13,13 +15,12 @@ function generateGallery(album, production, id, photopath, url) {
 		for (let i = 0; i < imagesNames.length; i++) {
 			let filename = file.images[imagesNames[i]]?.ext ?
 				imagesNames[i] + "." +file.images[imagesNames[i]].ext : imagesNames[i] + ".jpg";
-			let path = album.substring(0, album.lastIndexOf("/"));
-			let dev = `<img src="${path}/${filename}" width="500"/>`;
+			let dev = `<img src="${imagePath}/${filename}" width="500"/>`;
 			let prod =
-				`<img src="//images.weserv.nl/?url=${url}${path}/${filename}&w=500&output=jpg&q=90" alt="${filename}" />`;
+				`<img src="//images.weserv.nl/?url=${url}${imagePath}/${filename}&w=500&output=jpg&q=90" alt="${filename}" />`;
 
 			albumDiv.innerHTML += `
-				<a href="${photopath}?photo=${path}/${filename}" class="no-lightbox">
+				<a href="${photopath}?photo=${imagePath}/${filename}" class="no-lightbox">
 				${production ? prod : dev}
 </a>
 			`
