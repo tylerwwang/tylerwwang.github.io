75796ff94e880695f99d332b6a590cad0b2864dc
Tyler Wang <wangtyler123@gmail.com>
2025-04-05 23:07:10 -0500

    Modified footnotes

--- js/footnote.js
+++ js/footnote.js
@@ -7,31 +7,65 @@ window.addEventListener("DOMContentLoaded",
 	() => {
 		let fn = document.getElementsByClassName('foot-tag');
 
+		if(fn.length == 0) {
+			document.getElementById("footnote-box").remove();
+			return;
+		}
+
 		[...fn].forEach(function(el) {
 			let button = document.createElement('span');
+			let index = footnotelist.length + 1;
+
 			footnotelist.push(el.innerHTML);
 
-			button.innerHTML = `<button class="footnote-button">${footnotelist.length}</button>`;
+			button.innerHTML = `<button class="footnote-button">${index}</button>`;
 			button.classList.add('footnote');
+			button.id = `footnote-tag-number-${index}`;
 
 			el.parentNode.replaceChild(button, el);
 
 			button.addEventListener("click", function() {
-				document.getElementById(`footnote-number-${footnotelist.length}`)?.scrollIntoView({block: "center", behavior: "smooth"});
+				let footnote = document.getElementById(`footnote-number-${index}`);
+
+				footnote?.scrollIntoView({block: "center", behavior: "smooth"});
+
+				footnote.classList.remove("footnote-animate");
+				void footnote.offsetWidth;
+				footnote.classList.add("footnote-animate");
 			});
 		});
 
 		if (footnotelist.length > 0) {
-			let wrapper = document.getElementById('main-page-wrapper');
+			let box = document.getElementById('footnote-box');
 			let footnotes = document.createElement('ol');
+
+			box.innerHTML = "";
 
 			footnotes.classList.add('footnotes');
 
 			[... footnotelist].forEach((note, i) => {
-				footnotes.innerHTML += `<li id='footnote-number-${i+1}'>${note}</li>`;
+				let item = document.createElement('li');
+				item.id = `footnote-number-${i+1}`;
+				item.innerHTML = `<span id="footnote-number-${i+1}-span">${note}</span>&nbsp`;
+				footnotes.appendChild(item);
+
+				let button = document.createElement('button');
+				button.classList.add('footnote-back-button');
+				button.innerHTML = "&#8617;";
+				button.addEventListener("click", function() {
+					let footnoteTag = document.getElementById(`footnote-tag-number-${i+1}`);
+
+					footnoteTag?.scrollIntoView({block: "center", behavior: "smooth"});
+
+					footnoteTag.classList.remove("footnote-tag-animate");
+					void footnoteTag.offsetWidth;
+					footnoteTag.classList.add("footnote-tag-animate");
+				});
+
+				item.appendChild(button);
 			});
 
-			wrapper.appendChild(footnotes);
+			box.appendChild(footnotes);
 		}
 	}
 )
